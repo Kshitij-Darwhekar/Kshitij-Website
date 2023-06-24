fa4b59ee37ab4722e6c81eb20b7f979616b73eae
kshitij <kshitijdarwhekar@gmail.com>
2023-06-24 13:59:58 +0530

    Projects Added

--- portfolio-website/src/app/page.js
+++ portfolio-website/src/app/page.js
@@ -8,12 +8,12 @@ import kshitij from '../../public/Kshitij.png';
 import design from '../../public/design.png';
 import code from '../../public/code.png';
 import consulting from '../../public/consulting.png';
-import web1 from '../../public/web1.png';
-import web2 from '../../public/web2.png';
-import web3 from '../../public/web3.png';
-import web4 from '../../public/web4.png';
-import web5 from '../../public/web5.png';
-import web6 from '../../public/web6.png';
+import pizza from '../../public/pizza1.png';
+import Traffic from '../../public/Traffic.png';
+import FlySmart from '../../public/FlySmart2.png';
+import portfolio from '../../public/Portfolio1.png';
+
+
 import { useState } from 'react';
 // import Skills from './skills';
 import Link from 'next/link';
@@ -60,10 +60,12 @@ export default function Home() {
           
             <div className='text-center p-10 '>
               <h2 className='text-5xl py-10 text-teal-600 font-medium dark:text-gradient-to-r from-cyan-700 to-cyan-500 md:text-6xl'>Kshitij Darwhekar</h2>
-              <h3 className='text-2xl py-2 dark:text-white md:text-3xl'>Developer and ML Enthusiast</h3>
+              <h3 className='text-2xl py-2 dark:text-white md:text-3xl'>Full Stack Developer and ML Enthusiast</h3>
               <p className='text-md py-5 leading-8 text-gray-800 dark:text-gray-200
-              md:text-xl max-w-xl mx-auto'>
-                I am a 3rd year student at IIT Kharagpur. I am interested in Web Development and Machine Learning. I am currently working on a project to detect and classify skin diseases using Deep Learning. 
+              md:text-xl max-w-10xl mx-auto'>
+               I am a full stack developer with experience in Angular, React, JavaScript, Java, Spring Boot, MongoDB, Express, Node.js, Python, data science, machine learning, and deep learning. 
+               I have a strong background in Electronics and Telecommunication Engineering, enabling me to build robust web applications and analyze large datasets using Python-based data science techniques.
+               I am passionate about leveraging my diverse skill set to contribute to organizations in improving decision-making processes and developing data-driven solutions.
               </p>
               <div className='text-5xl flex justify-center gap-16 py-3 text-gray-600 dark:text-gray-400'>
                 <a href='https://twitter.com/KshitijDarwhek1'><AiFillTwitterCircle /></a>
@@ -134,30 +136,35 @@ export default function Home() {
           <section >
             <div>
               <h3 className='text-5xl py-3 mb-6 dark:text-white'>Services I Offer</h3>
-              <p className='text-md py-2 leading-8 text-grey-800 dark:text-gray-200'>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.
-                Lorem ipsum <span className='text-teal-500'>dolor sit amet </span> consectetur adipisicing elit. Quisquam, voluptatum.
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.
+              <p className='text-2xl py-2 leading-8 text-grey-800 dark:text-gray-200'>
+                
+                  As a full stack developer with expertise in Angular, React, JavaScript, Spring Boot, MongoDB, Express, Node.js, Python, Data Science, Machine Learning, and 
+                  Deep Learning, I offer the following services:
 
               </p>
-              <p className='text-md py-2 leading-8 text-grey-800 dark:text-gray-200'>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.
-                Lorem ipsum <span className='text-teal-500'>dolor sit amet </span> consectetur adipisicing elit. Quisquam, voluptatum.
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.
-
-              </p>
+              
+                <ul className='text-lg py-2 leading-8 text-grey-800 dark:text-gray-200 text-justify list-disc ml-9 '>
+                  <li className='my-4'> <span className='text-cyan-500'>Full Stack Web Development  </span> : I can design and develop responsive web applications using modern frameworks like Angular and React, backed by efficient server-side implementations using Spring Boot, Express, and Node.js.</li>
+                  <li className='my-4'> <span className='text-cyan-500'>Data Analysis and Extraction  </span> : With my knowledge in Python, data science, and machine learning, I can assist businesses in extracting meaningful insights from large datasets, performing exploratory data analysis, and developing predictive models. </li>
+                  <li className='my-4'> <span className='text-cyan-500'>Database Design and Implementation  </span>: I can design and implement efficient database solutions using MongoDB, ensuring data integrity and scalability for web applications and data-driven systems</li>
+                  <li className='my-4'> <span className='text-cyan-500'>API Development  </span>: I have experience in building RESTful APIs using frameworks like Express and Spring Boot, enabling seamless communication between front-end and back-end systems.</li>
+                  <li className='my-4'>  <span className='text-cyan-500'>Technical Consulting  </span> : I can provide technical guidance and consulting services, helping businesses make informed decisions regarding technology stack selection, architecture design, and best practices for software development projects.</li>
+                  <li className='my-4'>  <span className='text-cyan-500'>Continuous Improvement and Optimization  </span> : I am dedicated to continuously improving the performance and scalability of applications, optimizing code, and staying updated with the latest industry trends and best practices.</li>
+                </ul>
               
             </div>
             <div className='lg:flex gap-10'>
               <div className='text-center shadow-lg  p-10 rounded-xl my-10 dark:bg-gradient-to-b from-cyan-800 to-cyan-500'>
                 <div className='flex justify-center flex-col items-center '>
                   <Image src={design} width={100} height={100}/>
-                  <h3 className='text-lg font-medium pt-8 pb-2'>Beautiful Designs</h3>
-                  <p className='py-2'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.</p>
-                  <h4 className='py-4 text-cyan-500 dark:text-gray-300'>Design tools I Use</h4>
-                  <p className='text=grey-800 py-1'>Photoshop</p>
-                  <p className='text=grey-800 py-1'>Figma</p>
-                  <p className='text=grey-800 py-1'>Adobe XD</p>
+                  <h3 className='text-lg font-medium pt-8 pb-2'>Full Stack Web Development</h3>
+                  <p className='py-2'>Front-end and back-end development, covering the complete technology stack from user interface to server-side logic and database management. </p>
+                  <h4 className='py-4 text-cyan-500 dark:text-gray-300'>Technologies I Use</h4>
+                  <p className='text=grey-800 py-1'>React</p>
+                  <p className='text=grey-800 py-1'>Spring Boot</p>
+                  <p className='text=grey-800 py-1'>MongoDB</p>
+                  <p className='text=grey-800 py-1'>Express Js</p>
+                  <p className='text=grey-800 py-1'>Node Js</p>
 
                 </div>
               </div>
@@ -166,12 +173,14 @@ export default function Home() {
               <div className='text-center shadow-lg  p-10 rounded-xl my-10 dark:bg-gradient-to-b from-cyan-800 to-cyan-500'>
                 <div className='flex justify-center flex-col items-center '>
                   <Image src={code} width={100} height={100}/>
-                  <h3 className='text-lg font-medium pt-8 pb-2'>Beautiful Designs</h3>
-                  <p className='py-2'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.</p>
-                  <h4 className='py-4 text-cyan-500 dark:text-gray-300'>Design tools I Use</h4>
-                  <p className='text=grey-800 py-1'>Photoshop</p>
-                  <p className='text=grey-800 py-1'>Figma</p>
-                  <p className='text=grey-800 py-1'>Adobe XD</p>
+                  <h3 className='text-lg font-medium pt-8 pb-2'>Machine Learning</h3>
+                  <p className='py-2'>Developing algorithms and models that enable computers to learn and make predictions or decisions without being explicitly programmed.</p>
+                  <h4 className='py-4 text-cyan-500 dark:text-gray-300'>Technologies I Use</h4>
+                  <p className='text=grey-800 py-1'>Python</p>
+                  <p className='text=grey-800 py-1'>Pandas</p>
+                  <p className='text=grey-800 py-1'>Numpy</p>
+                  <p className='text=grey-800 py-1'>Matplotlib</p>
+                  <p className='text=grey-800 py-1'>Seaborn</p>
                 </div>
               </div>
             
@@ -180,12 +189,15 @@ export default function Home() {
               <div className='text-center shadow-lg  p-10 rounded-xl my-10 dark:bg-gradient-to-b from-cyan-800 to-cyan-500'>
                 <div className='flex justify-center flex-col items-center '>
                   <Image src={consulting} width={100} height={100}/>
-                  <h3 className='text-lg font-medium pt-8 pb-2'>Beautiful Designs</h3>
-                  <p className='py-2'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.</p>
-                  <h4 className='py-4 text-cyan-500 dark:text-gray-300'>Design tools I Use</h4>
-                  <p className='text=grey-800 py-1'>Photoshop</p>
-                  <p className='text=grey-800 py-1'>Figma</p>
-                  <p className='text=grey-800 py-1'>Adobe XD</p>
+                  <h3 className='text-lg font-medium pt-8 pb-2'>Software Development</h3>
+                  <p className='py-2'>Designing, coding, testing, and maintaining computer programs and applications to meet specific user or business requirements.</p>
+                  <h4 className='py-4 text-cyan-500 dark:text-gray-300'>Technologies I Use</h4>
+                  <p className='text=grey-800 py-1'>Java</p>
+                  <p className='text=grey-800 py-1'>GitHub</p>
+                  <p className='text=grey-800 py-1'>Docker</p>
+                  <p className='text=grey-800 py-1'>Kubernetes</p>
+                  {/* <p className='text=grey-800 py-1'>Ansimble</p> */}
+                  <p className='text=grey-800 py-1'>Jenkins</p>
 
                 </div>
               </div>
@@ -196,27 +208,25 @@ export default function Home() {
             <div>
               <h3 className='text-5xl py-2 mb-6 dark:text-white'>Portfolio</h3>
               <p className='text-md py-2 leading-8 text-grey-800 dark:text-gray-200'>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.
-                Lorem ipsum <span className='text-teal-500'>dolor sit amet </span> consectetur adipisicing elit. Quisquam, voluptatum.
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.
+              My portfolio is a collection of diverse work samples that reflect my expertise and experience in <span className='text-cyan-500'> Full Stack development and Machine Learning </span> . 
+              It includes a range of projects, such as <span className='text-cyan-500'> Pizzeria-react-app , FlySmart , Medical Insurance Premium prediction , Intelligent Traffic Managment System, Portfolio Website </span>  
+              each demonstrating my creativity, problem-solving abilities, and professional growth.
               </p>
               <p className='text-md py-2 leading-8 text-grey-800 dark:text-gray-200'>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.
-                Lorem ipsum <span className='text-teal-500'>dolor sit amet </span> consectetur adipisicing elit. Quisquam, voluptatum.
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.
+              Through my portfolio, I aim to present the quality of my work and provide potential employers, clients, or collaborators 
+              with a comprehensive understanding of my capabilities. 
+              It not only highlights my past achievements but also offers insights into my thought process, 
+              approach to projects, and overall professional development. By exploring my portfolio, 
+              you will gain a deeper understanding of my skills and be able to assess my suitability for your specific needs.
               </p>
             </div>
             
             <div className='flex flex-col gap-10 py-10 lg:flex-row lg:flex-wrap'>
-              <div className='basis-1/3 flex-1 '><Image src={web1} className='rounded-lg object-cover ' width={'100%'} height={'100%'} style={portfolioStyle}/></div>
-              <div className='basis-1/3 flex-1 '><Image src={web2} className='rounded-lg object-cover ' width={'100%'} height={'100%'} style={portfolioStyle}/></div>
-              <div className='basis-1/3 flex-1 '><Image src={web3} className='rounded-lg object-cover ' width={'100%'} height={'100%'} style={portfolioStyle}/></div>
-              <div className='basis-1/3 flex-1 '><Image src={web4} className='rounded-lg object-cover ' width={'100%'} height={'100%'} style={portfolioStyle}/></div>
-              <div className='basis-1/3 flex-1 '><Image src={web5} className='rounded-lg object-cover ' width={'100%'} height={'100%'} style={portfolioStyle}/></div>
-              <div className='basis-1/3 flex-1 '><Image src={web6} className='rounded-lg object-cover ' width={'100%'} height={'100%'} style={portfolioStyle}/></div>
+
+              <div className='basis-1/3 flex-1 '><a href='https://github.com/Kshitij-Darwhekar/pizzeria-react-app'><Image src={pizza} className='rounded-lg object-cover ' width={'100%'} height={'100%'} style={portfolioStyle}/></a></div>
+              <div className='basis-1/3 flex-1 '><a href='https://github.com/Kshitij-Darwhekar/kshitijdarwhekar.io'><Image src={portfolio} className='rounded-lg object-cover ' width={'100%'} height={'100%'} style={portfolioStyle}/></a></div>
+              <div className='basis-1/3 flex-1 '><a href='https://github.com/WebDesgns/Flight-Management-System'><Image src={FlySmart} className='rounded-lg object-cover ' width={'100%'} height={'100%'} style={portfolioStyle}/></a></div>
+              <div className='basis-1/3 flex-1 '><a href='https://github.com/Kshitij-Darwhekar/Intelligent-Traffic-Managment-System-Using-Computer-Vision'><Image src={Traffic} className='rounded-lg object-cover ' width={'100%'} height={'100%'} style={portfolioStyle}/></a></div>
             </div>
 
           </section>
@@ -225,11 +235,11 @@ export default function Home() {
             <form>
               <h1 className='text-4xl dark:text-gray-200 py-5'>Contact Me</h1>
               <label htmlFor='name' className='dark:text-gray-200'>Name</label>
-              <input type='text' id='name' className='border-2 border-gray-300 p-2 rounded-lg w-full dark:bg-gradient-to-b from-cyan-700 to-cyan-500 dark:text-gray-200'/>
+              <input type='text' id='name' className='border-2 border-gray-300 p-2 rounded-lg w-full '/>
               <label htmlFor='email' className='dark:text-gray-200'>Email</label>
-              <input type='email' id='email' className='border-2 border-gray-300 p-2 rounded-lg w-full dark:bg-gradient-to-b from-cyan-700 to-cyan-500 dark:text-gray-200'/>
+              <input type='email' id='email' className='border-2 border-gray-300 p-2 rounded-lg w-full '/>
               <label htmlFor='message' className='dark:text-gray-200'>Message</label>
-              <textarea name='message' id='message' cols='30' rows='10' className='border-2 border-gray-300 p-2 py-4 rounded-lg w-full dark:bg-gradient-to-b from-cyan-700 to-cyan-500 dark:text-gray-200 '></textarea>
+              <textarea name='message' id='message' cols='30' rows='10' className='border-2 border-gray-300 p-2 py-4 rounded-lg w-full '></textarea>
               <button onClick={handleSubmit} className='bg-gradient-to-r from-cyan-700 to-cyan-500  text-white px-4 py-2 rounded-lg'>Submit</button>
             </form>
           </section>
