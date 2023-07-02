460da82b8f616c2d6ae36f2d4c1bcb5c13d3360a
kshitij <kshitijdarwhekar@gmail.com>
2023-07-02 14:06:11 +0530

    CName added

--- portfolio-website/src/app/layout.js
+++ portfolio-website/src/app/layout.js
@@ -6,7 +6,8 @@ const dancingScript = Dancing_Script({ subsets: ['latin'] })
 
 export const metadata = {
   title: "Kshitij's Portfolio",
-  description: "Explore the diverse creations of a skilled full stack developer. Discover a showcase of innovative web and software projects on my portfolio website. From front-end design to back-end development, witness the power of seamless integration and captivating user experiences. Unleash the potential of technology and witness the artistry of coding. Visit now and embark on a journey through my digital world."}
+  description: "Explore the diverse creations of a skilled full stack developer. Discover a showcase of innovative web and software projects on my portfolio website. From front-end design to back-end development, witness the power of seamless integration and captivating user experiences. Unleash the potential of technology and witness the artistry of coding. Visit now and embark on a journey through my digital world."
+}
 
 export default function RootLayout({ children }) {
   return (
