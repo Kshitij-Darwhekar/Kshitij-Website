2b6ae145664840d9e4c6975e40c848ab5528a903
Kshitij Darwhekar <kshitijdarwhekar@gmail.com>
2024-07-02 16:38:22 +0530

    Caret Color Changed

--- portfolio-website/src/app/page.js
+++ portfolio-website/src/app/page.js
@@ -84,7 +84,6 @@ export default function Home() {
                   <button onClick={() => {contactRef.current?.scrollIntoView({behavior: 'smooth' })} } className='hover:text-teal-500 text-lg'> Contact </button>
                 </ul>
                
-                
               </nav>
               <ul className='flex items-center justify-end'>
                 <li><span className=''> <BsFillMoonStarsFill onClick={()=> setDarkMode(!darkMode)} className='cursor-pointer text-2xl dark:text-slate-400 hover:text-teal-500 dark:hover:text-teal-500' /></span> </li>
@@ -352,12 +351,12 @@ export default function Home() {
           <h1 className='text-5xl dark:text-slate-400 py-5'>Contact Me</h1>
             <form onSubmit={handleSubmit} className='p-3'>
               <label htmlFor='name' className='dark:text-slate-400 '>Name</label>
-              <input type='name' id='name' name='name' required className='my-2 border-2 border-teal-500 p-2 rounded-lg w-full dark:border-teal-500 dark:bg-black dark:text-gray-200' />
+              <input type='name' id='name' name='name' required className='my-2 border-2 border-teal-500 p-2 rounded-lg w-full caret-teal-500 dark:border-teal-500 dark:bg-black dark:text-gray-200' />
               <label htmlFor='email' className='dark:text-slate-400 '>Email</label>
-              <input type='email' id='email' name='email' required className='my-2 border-2 border-teal-500 p-2 rounded-lg w-full dark:border-teal-500 dark:bg-black dark:text-gray-200 '/>
+              <input type='email' id='email' name='email' required className='my-2 border-2 border-teal-500 p-2 rounded-lg w-full caret-teal-500 dark:border-teal-500 dark:bg-black dark:text-gray-200 '/>
               <ValidationError prefix="Email" field="email" errors={state.errors}/>
               <label htmlFor='message' className='dark:text-slate-400 '>Message</label>
-              <textarea name='message' id='message' cols='30' rows='10' className='my-2 border-2 border-teal-500  p-2 py-4 rounded-lg w-full dark:border-teal-500 dark:bg-black dark:text-gray-200 '></textarea>
+              <textarea name='message' id='message' cols='30' rows='10' className='my-2 border-2 border-teal-500 p-2 py-4 rounded-lg w-full caret-teal-500 dark:border-teal-500 dark:bg-black dark:text-gray-200 '></textarea>
               <ValidationError prefix="Message" field="message" errors={state.errors}/>
               {/* <button type='submit' disabled={state.submitting} className='bg-gradient-to-r from-teal-700 to-teal-500 my-2 hover:bg-gradient-to-l text-slate-200 px-4 py-2 rounded-lg'>Submit</button> */}
               <button type='submit' className="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-teal-700 to-teal-500 group-hover:from-cyan-500 group-hover:to-cyan-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-cyan-200 dark:focus:ring-cyan-800">
